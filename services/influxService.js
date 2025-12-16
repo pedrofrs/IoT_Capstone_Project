47afd9dff401efe781d7b8c3a794813e048b08d1
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-12-16 14:47:12 -0300

    feat: refatocao servico banco de dados

--- services/influxService.js
+++ services/influxService.js
@@ -1,33 +1,63 @@
 const { InfluxDB, Point } = require("@influxdata/influxdb-client");
 require("dotenv").config();
 
-const influx = new InfluxDB({
+const influxDB = new InfluxDB({
     url: process.env.INFLUX_URL,
-    token: process.env.INFLUX_TOKEN,
+    token: process.env.INFLUX_TOKEN
 });
 
-const writeApi = influx.getWriteApi(
-    process.env.INFLUX_ORG,
-    process.env.INFLUX_BUCKET,
-    "ns"
-);
+const ORG = process.env.INFLUX_ORG;
+const BUCKET = process.env.INFLUX_BUCKET;
 
-function salvarTelemetria(t) {
+
+async function testarConexaoEEscrita() {
+    const writeApi = influxDB.getWriteApi(ORG, BUCKET);
+
     try {
-        const point = new Point("warehouse_env")
-            .tag("deviceId", t.deviceId || "device-001")
-            .floatField("temperatura", t.temp)
-            .floatField("umidade", t.hum)
-            .intField("ldr", t.ldr || 0)
-            .booleanField("alertTemp", t.alertTemp)
-            .booleanField("alertHum", t.alertHum)
-            .booleanField("alertAny", t.alertAny)
-            .timestamp(new Date(t.ts || Date.now()));
+        const point = new Point("health_check")
+            .tag("service", "sensor-api")
+            .floatField("status", 1)
+            .timestamp(new Date());
 
         writeApi.writePoint(point);
-    } catch (err) {
-        console.error("Erro ao salvar no InfluxDB:", err.message);
+
+        await writeApi.close();
+
+        return {
+            status: "OK",
+            mensagem: "Conexão e escrita no InfluxDB funcionando"
+        };
+
+    } catch (error) {
+        throw new Error(`Erro ao escrever no InfluxDB: ${error.message}`);
     }
 }
 
-module.exports = { salvarTelemetria };
+
+async function salvarTelemetria(dados) {
+    const writeApi = influxDB.getWriteApi(ORG, BUCKET);
+
+    try {
+        const point = new Point("warehouse_env")
+            .tag("deviceId", dados.deviceId || "device-001")
+            .floatField("temperatura", dados.temp)
+            .floatField("umidade", dados.hum)
+            .intField("ldr", dados.ldr || 0)
+            .booleanField("alertTemp", dados.alertTemp)
+            .booleanField("alertHum", dados.alertHum)
+            .booleanField("alertAny", dados.alertAny)
+            .timestamp(new Date(dados.ts || Date.now()));
+
+        writeApi.writePoint(point);
+
+        await writeApi.close();
+
+    } catch (error) {
+        console.error("Erro ao salvar telemetria:", error.message);
+    }
+}
+
+module.exports = {
+    testarConexaoEEscrita,
+    salvarTelemetria
+};
