2e5fdc87cce3b9569f3c2d82c1377114c948cde8
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-12-16 20:43:54 -0300

    refact: refatocao da logica de abertura de conexao com banco banco de dados para evitar erros de timeout

--- services/influxService.js
+++ services/influxService.js
@@ -3,40 +3,18 @@ require("dotenv").config();
 
 const influxDB = new InfluxDB({
     url: process.env.INFLUX_URL,
-    token: process.env.INFLUX_TOKEN
+    token: process.env.INFLUX_TOKEN,
+    timeout: 20_000
 });
 
 const ORG = process.env.INFLUX_ORG;
 const BUCKET = process.env.INFLUX_BUCKET;
 
+const writeApi = influxDB.getWriteApi(ORG, BUCKET, "ns");
 
-async function testarConexaoEEscrita() {
-    const writeApi = influxDB.getWriteApi(ORG, BUCKET);
+writeApi.useDefaultTags({ app: "sensor-api" });
 
-    try {
-        const point = new Point("health_check")
-            .tag("service", "sensor-api")
-            .floatField("status", 1)
-            .timestamp(new Date());
-
-        writeApi.writePoint(point);
-
-        await writeApi.close();
-
-        return {
-            status: "OK",
-            mensagem: "Conexão e escrita no InfluxDB funcionando"
-        };
-
-    } catch (error) {
-        throw new Error(`Erro ao escrever no InfluxDB: ${error.message}`);
-    }
-}
-
-
-async function salvarTelemetria(dados) {
-    const writeApi = influxDB.getWriteApi(ORG, BUCKET);
-
+function salvarTelemetria(dados) {
     try {
         const point = new Point("warehouse_env")
             .tag("deviceId", dados.deviceId || "device-001")
@@ -50,14 +28,22 @@ async function salvarTelemetria(dados) {
 
         writeApi.writePoint(point);
 
-        await writeApi.close();
-
     } catch (error) {
-        console.error("Erro ao salvar telemetria:", error.message);
+        console.error("Erro ao escrever ponto no Influx:", error.message);
+    }
+}
+
+
+async function encerrarInflux() {
+    try {
+        await writeApi.close();
+        console.log("Writer do InfluxDB fechado corretamente");
+    } catch (err) {
+        console.error("Erro ao fechar writer do Influx:", err.message);
     }
 }
 
 module.exports = {
-    testarConexaoEEscrita,
-    salvarTelemetria
+    salvarTelemetria,
+    encerrarInflux
 };
