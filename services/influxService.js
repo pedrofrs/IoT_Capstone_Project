149cbea5be06a0e1d2c4ecd967a5d3c87b0a87b5
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-12-16 20:56:11 -0300

    refact: t

--- services/influxService.js
+++ services/influxService.js
@@ -4,55 +4,28 @@ require("dotenv").config();
 const influxDB = new InfluxDB({
     url: process.env.INFLUX_URL,
     token: process.env.INFLUX_TOKEN,
-    timeout: 50_000
+    timeout: 30_000
 });
 
-const ORG = process.env.INFLUX_ORG;
-const BUCKET = process.env.INFLUX_BUCKET;
-
 const writeApi = influxDB.getWriteApi(
-    ORG,
-    BUCKET,
+    process.env.INFLUX_ORG,
+    process.env.INFLUX_BUCKET,
     "ns",
     {
         batchSize: 1,
         flushInterval: 1_000,
-        maxRetries: 3,
-        maxRetryDelay: 5_000
+        maxRetries: 3
     }
 );
-writeApi.useDefaultTags({ app: "sensor-api" });
 
 function salvarTelemetria(dados) {
-    try {
-        const point = new Point("warehouse_env")
-            .tag("deviceId", dados.deviceId || "device-001")
-            .floatField("temperatura", dados.temp)
-            .floatField("umidade", dados.hum)
-            .intField("ldr", dados.ldr || 0)
-            .booleanField("alertTemp", dados.alertTemp)
-            .booleanField("alertHum", dados.alertHum)
-            .booleanField("alertAny", dados.alertAny)
-            .timestamp(new Date(dados.ts || Date.now()));
+    const point = new Point("warehouse_env")
+        .floatField("temperatura", dados.temp)
+        .floatField("umidade", dados.hum)
+        .booleanField("alertAny", dados.alertAny)
+        .timestamp(new Date());
 
-        writeApi.writePoint(point);
-
-    } catch (error) {
-        console.error("Erro ao escrever ponto no Influx:", error.message);
-    }
+    writeApi.writePoint(point);
 }
 
-
-async function encerrarInflux() {
-    try {
-        await writeApi.close();
-        console.log("Writer do InfluxDB fechado corretamente");
-    } catch (err) {
-        console.error("Erro ao fechar writer do Influx:", err.message);
-    }
-}
-
-module.exports = {
-    salvarTelemetria,
-    encerrarInflux
-};
+module.exports = { salvarTelemetria };
