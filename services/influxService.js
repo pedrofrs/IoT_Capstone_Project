1e6f01975221f422132973d5b9dedd54021f8ad3
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-12-16 23:03:25 -0300

    feat: influxDB implementacao

--- services/influxService.js
+++ services/influxService.js
@@ -1,24 +1,32 @@
-const Telemetria = require("../models/Telemetria");
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+require("dotenv").config();
 
-async function salvarTelemetria(t) {
+const influx = new InfluxDB({
+    url: process.env.INFLUX_URL,
+    token: process.env.INFLUX_TOKEN,
+});
+
+const writeApi = influx.getWriteApi(
+    process.env.INFLUX_ORG,
+    process.env.INFLUX_BUCKET,
+    "ns"
+);
+
+function salvarTelemetria(t) {
     try {
-        const registro = await Telemetria.create({
-            deviceId: t.deviceId || "device-001",
-            temperatura: t.temp,
-            umidade: t.hum,
-            ldr: t.ldr || 0,
+        const point = new Point("warehouse_env")
+            .tag("deviceId", t.deviceId || "device-001")
+            .floatField("temperatura", t.temp)
+            .floatField("umidade", t.hum)
+            .intField("ldr", t.ldr || 0)
+            .booleanField("alertTemp", t.alertTemp)
+            .booleanField("alertHum", t.alertHum)
+            .booleanField("alertAny", t.alertAny)
+            .timestamp(new Date(t.ts || Date.now()));
 
-            alertTemp: t.alertTemp,
-            alertHum: t.alertHum,
-            alertAny: t.alertAny,
-
-            timestamp: t.ts ? new Date(t.ts) : new Date()
-        });
-
-        return registro;
+        writeApi.writePoint(point);
     } catch (err) {
-        console.error("Erro ao salvar telemetria no MongoDB:", err.message);
-        throw err;
+        console.error("Erro ao salvar no InfluxDB:", err.message);
     }
 }
 
