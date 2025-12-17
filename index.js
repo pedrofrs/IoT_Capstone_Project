f2664e6a409b9a2a3986f634e4137d5b93796ece
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-12-16 21:01:01 -0300

    refact: index.js rollback

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 require("dotenv").config();
 const cors = require('cors');
 const sensorRoutes = require('./routes/sensorRoutes');
-const { encerrarInflux } = require('./services/influxService');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('API IoT Online! Use /api/sensores para envio.');
@@ -17,29 +17,8 @@ app.get('/', (req, res) => {
 app.use('/api/sensores', sensorRoutes);
 
 const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
-async function shutdown(signal) {
-    console.log(`\nRecebido sinal ${signal}. Encerrando aplicação...`);
-
-    try {
-        await encerrarInflux();
-        console.log("InfluxDB finalizado com sucesso");
-    } catch (err) {
-        console.error("Erro ao encerrar InfluxDB:", err.message);
-    }
-
-    server.close(() => {
-        console.log("Servidor HTTP encerrado");
-        process.exit(0);
-    });
-}
-
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
 
 module.exports = app;
